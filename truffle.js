e8473047dc326c366b41bc7af168450db6bc57b3
Unknown <joshquin@gmail.com>
2017-11-05 17:20:38 -0500

    Reactiviated solC optimizer

--- truffle.js
+++ truffle.js
@@ -3,8 +3,18 @@ module.exports = {
         development: {
             host: "localhost",
             port: 8545,
-            network_id: "*", // Match any network id
-            gas: 500000
+            network_id: "*" // Match any network id
+        }
+    },
+    solc: {
+        // Turns on the Solidity optimizer. For smart contract auditing, be
+        // sure to turn this off. For more information, see the Truffle 4.0.0
+        // release notes.
+        //
+        // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
+        optimizer: {
+            enabled: true,
+            runs: 200
         }
     }
 };
